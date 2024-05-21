edd8f390e9ec78566e90ddd7c3b80f64994791fa
Jonas Petrik <jonas@petrik.dev>
2024-05-21 22:39:40 +0200

    Sourcemap wizard

--- src/app/global-error.jsx
+++ src/app/global-error.jsx
@@ -5,9 +5,9 @@ import Error from "next/error";
 import { useEffect } from "react";
 
 export default function GlobalError({ error }) {
-  useEffect(() => {
-    Sentry.captureException(error);
-  }, [error]);
+    useEffect(() => {
+        Sentry.captureException(error);
+    }, [error]);
 
   return (
     <html>
