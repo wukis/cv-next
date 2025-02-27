9463823abe8365f80bcd64281613217e1320cf2c
Jonas Petrik <jonas@petrik.dev>
2025-02-27 08:38:59 +0100

    Revert "Possible particles fix"
    This reverts commit a4ac30359f901ab2ccf0d5aa1e17ae9a269c9297.

--- src/components/ParticlesBackground.tsx
+++ src/components/ParticlesBackground.tsx
@@ -1,10 +1,11 @@
 'use client';
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState, lazy, Suspense } from "react";
 import { initParticlesEngine } from "@tsparticles/react";
-import { Particles } from "@tsparticles/react";
 import { loadAll } from "@tsparticles/all";
 import { type Container, type ISourceOptions } from "@tsparticles/engine";
-import ErrorBoundary from '@/components/ErrorBoundary';
+import ErrorBoundary from '@/components/ErrorBoundary';  // Import ErrorBoundary
+
+const Particles = lazy(() => import("@tsparticles/react").then(module => ({ default: module.Particles })));
 
 const ParticlesBackground = () => {
     const [init, setInit] = useState(false);
