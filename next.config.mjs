87319100105024597913b695db9aa2298dfce1d5
Jonas Petrik <jonas@petrik.dev>
2026-01-02 17:11:12 +0100

    small improvements

--- next.config.mjs
+++ next.config.mjs
@@ -19,7 +19,7 @@ const nextConfig = {
       // Reduce bundle size
       compiler: {
         removeConsole: process.env.NODE_ENV === 'production' ? {
-          exclude: ['error', 'warn'],
+          exclude: ['error', 'warn', 'log'], // Keep console.log for easter egg
         } : false,
       },
 }
