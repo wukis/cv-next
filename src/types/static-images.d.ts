f57ee52b4815c5a264ebb7b4c1c6f0bdbd2bdb62
Jonas Petrik <jonas@petrik.dev>
2026-03-16 20:54:39 +0100

    Pipeline fix

---
+++ src/types/static-images.d.ts
@@ -0,0 +1,29 @@
+declare module '*.png' {
+  const content: import('next/image').StaticImageData
+  export default content
+}
+
+declare module '*.jpg' {
+  const content: import('next/image').StaticImageData
+  export default content
+}
+
+declare module '*.jpeg' {
+  const content: import('next/image').StaticImageData
+  export default content
+}
+
+declare module '*.webp' {
+  const content: import('next/image').StaticImageData
+  export default content
+}
+
+declare module '*.avif' {
+  const content: import('next/image').StaticImageData
+  export default content
+}
+
+declare module '*.gif' {
+  const content: import('next/image').StaticImageData
+  export default content
+}
